c5d9126f7710abd39022d137afdc2fee961e62c2
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2024-04-15 19:13:28 +0200

    Conteo de Genes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Contar genes en un ADN
+        String ADN = "ATGATGATGATG";
+        int numGenes = contarGenes(ADN);
+        System.out.println("El número de genes en el ADN es: " + numGenes);
+
+
+    }
+
+    public static int contarGenes (String ADN) {
+
+        int c = 0;
+        int index = ADN.indexOf("ATG");
+
+        if (index == -1) {
+            return 0;
+        } else {
+            c++;
+            c += contarGenes(ADN.substring(index + 3));
         }
+
+        return c;
     }
 }
